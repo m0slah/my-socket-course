01deec7e63b24487595b3fdc5c929f0d8c0132bc
m0slah <muhammedslahqadir@gmail.com>
2024-11-09 14:07:16 +0300

    import packages

---
+++ index.js
@@ -0,0 +1,12 @@
+//1. packages
+
+import express from "express";
+
+import http from http;
+
+import { fileURLToPath } from "node:url"
+
+import {dirname, join } from "node:path"
+
+import { Server } from "socket.io";
+
