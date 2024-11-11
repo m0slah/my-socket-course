3daa5722e5cdb450d9a865eeb92f5ad425b2491a
m0slah <muhammedslahqadir@gmail.com>
2024-11-12 01:57:11 +0300

    Start the Server

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-//1. packages
+// 1. packages
 
 import express from "express";
 
-import http from http;
+import http from "node:http";
 
-import { fileURLToPath } from "node:url"
+import { fileURLToPath } from "node:url";
 
-import {dirname, join } from "node:path"
+import { dirname, join } from "node:path";
 
 import { Server } from "socket.io";
 
-//2. instances
+// 2. instances
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
- 
+
+// 3.serving HTML file
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.get("/", (req, res) => res.sendFile(join(__dirname, "index.html")));
+
+// 5. Start the Server
+const PORT = 3000;
+server.listen(PORT, () => {
+  `server running on port :${PORT}`;
+});
